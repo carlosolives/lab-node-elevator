e1eecf732e234d4671da8c4d21d9998a744b9c30
Carlos Olives <carlosolives@gmail.com>
2017-02-07 08:29:57 +0100

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 class Elevator {
   constructor(){
     this.floor      = 0;
@@ -5,15 +6,35 @@ class Elevator {
     this.requests   = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    let update = this.update();
+    const mainInterval=setInterval(function(){
+      return update;
+    }, 1000);
+   }
+  stop(nextFloor) {
+    cleanInterval(mainInterval);
+  }
+  update(){
+     this.log();
+   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp(){
+    this.floor++;
+  }
+  floorDown(){
+    this.floor--;
+   }
+  call(person){
+    this.requests.push(person);
+    this.floor = person.originFloor;
+    console.log(this.requests);
+
+  }
+  log() {
+    console.log("Direction:| Floor:"+this.floor );
+   }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,41 @@
+/*jshint esversion: 6*/
 const Elevator = require('./elevator.js');
+
+const Person = require('./person.js');
+
+
+let elevator = new Elevator();
+const maxFloor = elevator.MAXFLOOR;
+
+let namePerson  = "carlos";
+let originFloor = "5";
+let destinationFloor ="0";
+
+
+let person = new Person(namePerson, originFloor, destinationFloor);
+elevator.call(person);
+
+
+let request = elevator.requests.shift();
+
+if(request.originFloor < request.destinationFloor){
+  let origin = parseInt(request.originFloor);
+  let destination = parseInt(request.destinationFloor);
+
+  while(origin<destination && maxFloor>=destination){
+    elevator.floorUp();
+    origin++;
+  }
+
+}
+if(request.originFloor > request.destinationFloor){
+  let origin = parseInt(request.originFloor);
+  let destination = parseInt(request.destinationFloor);
+  while(origin>destination && destination>=0){
+    elevator.floorDown();
+    origin--;
+  }
+
+}
+//elevator.floorUp();
+elevator.start();
